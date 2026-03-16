73b78921af51fa134d8322fca6f7128f39584141
Mallareddy Purram <1Mpurra@seacoastbank.com>
2026-03-16 14:04:09 -0500

    updated import.tf

--- terraform/envs/prod/imports.tf
+++ terraform/envs/prod/imports.tf
@@ -4,12 +4,6 @@
 variable "subscription_id" {
   type = string
 }
-variable "resource_group_name" {
-  type = string
-}
-variable "api_management_name" {
-  type = string
-}
 
 
 ##############################################
